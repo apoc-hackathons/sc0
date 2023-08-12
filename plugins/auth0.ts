95fe7989a69c9b3347ec33d76b954d5005affe19
leen-neel <and24903@gmail.com>
2023-08-12 15:49:12 +0530

    added auth0

---
+++ plugins/auth0.ts
@@ -0,0 +1,15 @@
+import { createAuth0 } from "@auth0/auth0-vue";
+
+export default defineNuxtPlugin((nuxtApp) => {
+  const auth0 = createAuth0({
+    clientId: "QfjXdZ3fTwd7h2Uxu5f4MK0WHvsVyP9A",
+    domain: "dev-gxmeel3ks2k83oxu.us.auth0.com",
+    authorizationParams: {
+      redirect_uri: "http://localhost:3000/",
+    },
+  });
+
+  if (process.client) {
+    nuxtApp.vueApp.use(auth0);
+  }
+});
