b2ca42fbf54893d74b9edbef8bd3742061f6255c
leen-neel <and24903@gmail.com>
2023-08-13 08:01:58 +0530

    basic landing page lmaao

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,37 @@
 <template>
-  <div class="grid place-content-center mt-56">
-    <a href="/questions" class="btn btn-primary">Take test</a>
-  </div>
+  <client-only>
+    <div class="hero h-screen">
+      <div class="hero-content text-center">
+        <div class="max-w-md">
+          <h1 class="text-5xl font-bold text-discord-blurple">EduApp</h1>
+          <p class="text-3xl my-5">Tests made easy</p>
+          <a v-if="isAuthenticated" href="/guilds" class="mt-4 btn btn-primary"
+            >Take test</a
+          >
+          <button
+            v-if="!isAuthenticated"
+            @click="login"
+            class="mt-4 ml-3 btn btn-primary"
+          >
+            Login
+          </button>
+        </div>
+      </div>
+    </div>
+  </client-only>
 </template>
+
+<script setup lang="ts">
+  import { useAuth0 } from "@auth0/auth0-vue";
+
+  // Composition API
+  const auth0 = process.client ? useAuth0() : undefined;
+
+  const isAuthenticated = computed(() => {
+    return auth0?.isAuthenticated.value;
+  });
+
+  const login = async () => {
+    await auth0?.loginWithRedirect();
+  };
+</script>
